6f9d8eafc058bcd1e3e84ee8d15ea15447f57587
Sohail <reshmant90@gmail.com>
2025-08-13 15:01:03 +0530

    barely working

--- app/index.tsx
+++ app/index.tsx
@@ -1,141 +1,43 @@
 import "./global.css";
-import {StatusBar, Text, TouchableOpacity, View} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
-import Timer from '../utils/timer';
-import Keyboard from '../utils/keyboard';
-import {Picker} from "@react-native-picker/picker";
-import {useEffect, useState} from "react";
-import {Image} from "expo-image";
-import {easyWords, mediumWords, hardWords} from '@/utils/wordList';
+import Homepage from "@/app/homepage";
+import {createNativeStackNavigator} from "expo-router/build/fork/native-stack/createNativeStackNavigator";
+import StartPage from "@/app/startPage";
+import * as Font from 'expo-font';
+import {useState, useEffect} from "react";
+import {ActivityIndicator, View} from "react-native";
 
 export default function Index() {
-    const [score, setScore] = useState(0);
-    const [difficulty, setDifficulty] = useState('easy') ;
-    const [startTimer, setStartTimer] = useState(false);
-    const [incorrectGuesses, setIncorrectGuesses] = useState(6);
+    const Stack = createNativeStackNavigator();
 
-    const maxGuesses = 6;
-
-    const getRandomRiddle = () => {
-        let riddle = { word: '', hint: ''};
-        if(difficulty === 'hard') {
-            riddle = hardWords[Math.floor(Math.random() * hardWords.length)];
-            hardWords.splice(hardWords.indexOf(riddle), 1);
-            return riddle;
-        }
-        else if(difficulty === 'medium') {
-            riddle =  mediumWords[Math.floor(Math.random() * mediumWords.length)];
-            mediumWords.splice(mediumWords.indexOf(riddle), 1);
-            return riddle;
-        }
-        riddle = easyWords[Math.floor(Math.random() * easyWords.length)];
-        easyWords.splice(easyWords.indexOf(riddle), 1);
-        return riddle;
-    };
-
-    const [currentRiddle, setCurrentRiddle] = useState(getRandomRiddle());
-    const [userAnswer, setUserAnswer] = useState<string[]>([]);
+    const [fontsLoaded, setFontsLoaded] = useState(false);
 
     useEffect(() => {
-        setUserAnswer(Array(currentRiddle.word.length).fill(''));
-    }, [currentRiddle]);
-
-    const handleKeyPress = (letter: string) => {
-        const currentLetter = letter.toLowerCase();
-        const answerSize = currentRiddle.word.length;
-        const updatedAnswer = [...userAnswer];
-        if(currentRiddle.word.includes(currentLetter) && !userAnswer.includes(currentLetter)) {
-            for(let i = 0; i < answerSize; i++) {
-                if(currentRiddle.word[i] === currentLetter) {
-                    updatedAnswer[i] = currentLetter;
-                }
+        async function loadFonts() {
+            try {
+                await Font.loadAsync({
+                    'SRegular': require('../assets/fonts/SpaceMono-Regular.ttf'),
+                });
+                setFontsLoaded(true);
+            } catch (error) {
+                console.error('Error loading fonts:', error);
             }
         }
-        setUserAnswer(updatedAnswer);
-        checkAnswer(userAnswer.join(''));
-    };
 
-    const checkAnswer = (userAnswer : string) => {
-        if(userAnswer === currentRiddle.word) {
-            if(difficulty === 'easy') {
-                setScore(score + 10);
-            }
-            else if(difficulty === 'medium') {
-                setScore(score + 15);
-            }
-            setScore(score + 30);
-            console.log(score);
-            setCurrentRiddle(getRandomRiddle());
-            setUserAnswer(Array(currentRiddle.word.length).fill(''));
-        }
-    };
+        loadFonts();
+    }, []);
 
-    useEffect(() => {
-        console.log(userAnswer.join(''));
-    }, [userAnswer]);
-
-    const hangmanImages = [
-        require('../assets/images/hangman-0.png'),
-        require('../assets/images/hangman-1.png'),
-        require('../assets/images/hangman-2.png'),
-        require('../assets/images/hangman-3.png'),
-        require('../assets/images/hangman-4.png'),
-        require('../assets/images/hangman-5.png'),
-        require('../assets/images/hangman-6.png'),
-    ];
+    if (!fontsLoaded) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" color="#5D3FD3" />
+            </View>
+        );
+    }
 
     return (
-        <SafeAreaView>
-            <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
-            <View className="items-center justify-center bg-white py-1">
-                <Text className="text-4xl font-bold text-blue-500">
-                    HangMan Game
-                </Text>
-            </View>
-            <View className="flex-row justify-between ml-1 mr-1 px-1 py-2 bg-white">
-                <Text className="text-xl font-bold text-gray-600">
-                    Score: {score}
-                </Text>
-
-                <TouchableOpacity className="bg-gray-300 rounded-l-md">
-                    <Text className="text-gray-600 text-xl font-bold px-0.5">
-                        Skip Word
-                    </Text>
-                </TouchableOpacity>
-            </View>
-            <View className="bg-white justify-between px-1 ml-1 mr-1" style={{flexDirection:"row", alignItems:"center"}}>
-                <Text className="text-xl font-bold text-gray-600">
-                    Difficulty:
-                </Text>
-                <Picker
-                    selectedValue={difficulty}
-                    onValueChange={(itemValue) => setDifficulty(itemValue)}
-                    style={{ width: 130 }}
-                    itemStyle={{ fontSize: 18, fontWeight: 'bold', color: '#4B5563' }}
-                >
-                    <Picker.Item label="Easy" value="easy" />
-                    <Picker.Item label="Medium" value="medium" />
-                    <Picker.Item label="Hard" value="hard" />
-                </Picker>
-                <Text className="text-xl font-bold text-gray-600">
-                    Time Left:
-                </Text>
-                <Timer/>
-            </View>
-
-            <View className="items-center mt-4 justify-center">
-                <Image source={hangmanImages[incorrectGuesses]} style={{ width: 200, height: 200 }} />
-            </View>
-
-            <View className="items-center justify-center mt-2">
-                <Text className="text-xl font-bold text-center px-2">
-                    {currentRiddle.hint}
-                </Text>
-            </View>
-
-            <View className="items-center mt-5 justify-center">
-                <Keyboard onKeyPress={handleKeyPress} />
-            </View>
-        </SafeAreaView>
+        <Stack.Navigator initialRouteName="StartPage" screenOptions={{headerShown: false}}>
+            <Stack.Screen name="StartPage" component={StartPage} />
+            <Stack.Screen name="Homepage" component={Homepage} />
+        </Stack.Navigator>
     );
 };
